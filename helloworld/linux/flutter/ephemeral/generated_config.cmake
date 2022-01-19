786c93f1ee7b826fbe86c9abf7cc5e61e733936d
Diyp <diypa571@gmail.com>
2022-01-19 08:21:55 +0100

    Add files via upload

---
+++ helloworld/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/diypa571/flutter/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/diypa571/Desktop/Flutter-projects/HelloWorld/helloworld" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/diypa571/flutter/flutter"
+  "PROJECT_DIR=/home/diypa571/Desktop/Flutter-projects/HelloWorld/helloworld"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/diypa571/Desktop/Flutter-projects/HelloWorld/helloworld/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/diypa571/Desktop/Flutter-projects/HelloWorld/helloworld/lib/main.dart"
+)
